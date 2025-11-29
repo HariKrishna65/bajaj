1cad30fe01e6390005c70db0e133d7fb5e614968
Tondapu Harikrishna <155811883+HariKrishna65@users.noreply.github.com>
2025-11-30 03:30:41 +0530

    Add InputBody class and refactor DataPayload
    Added InputBody class for document input and removed duplicate DataPayload class definition.

--- app/schemas.py
+++ app/schemas.py
@@ -1,31 +1,36 @@
 from pydantic import BaseModel
 from typing import List
 
+
+class InputBody(BaseModel):
+    document: str  # PDF/IMG URL
+
+
 class BillItem(BaseModel):
     item_name: str
     item_amount: float
     item_rate: float
     item_quantity: float
 
+
 class PageLineItems(BaseModel):
     page_no: str
     page_type: str
     bill_items: List[BillItem]
+
+
+class DataPayload(BaseModel):
+    pagewise_line_items: List[PageLineItems]
+    total_item_count: int
+
 
 class TokenUsage(BaseModel):
     total_tokens: int
     input_tokens: int
     output_tokens: int
 
-class DataPayload(BaseModel):
-    pagewise_line_items: List[PageLineItems]
-    total_item_count: int
 
 class SuccessResponse(BaseModel):
     is_success: bool
     token_usage: TokenUsage
     data: DataPayload
-
-class ErrorResponse(BaseModel):
-    is_success: bool
-    message: str
